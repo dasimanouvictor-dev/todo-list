d644ab81ff26986ae4955e6faabef7fa6fe25fd2
dasimanouvictor-dev <dasimanouvictor@gmail.com>
2026-08-20 12:18:56 +0100

    my third commit where i finished the first stylizing part of my project and i started to make it more dynamic whit js

---
+++ function/dom.js
@@ -0,0 +1,11 @@
+export function createElement(tagName, attribut={}) {
+    const element = document.createElement(tagName)
+    
+    for (const [name, value] of Object.entries(attribut)) {
+        if(value !== false || value !== null) {
+            element.setAttribut(name,value)
+        }
+    }
+
+    return element
+}
